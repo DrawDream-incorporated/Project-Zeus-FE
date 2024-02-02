cf3e3c0c094ef847c2ff875a84e4d69578c2fe14
Han Kyung Sung <gksruddlakstp@gmail.com>
2024-02-01 21:25:58 -0800

    Hansung/remove eslint (#39)
    * Update issue templates
    
    * Update issue templates
    
    * Create eslint.yml
    
    * Hansung/add eslint (#19)
    
    * Updated .gitignore file
    
    * Add eslint file.
    
    ---------
    
    Co-authored-by: Han kyung Sung <hankyungsung@Hans-MacBook-Pro.local>
    
    * Hansung/add eslint (#20)
    
    * Updated .gitignore file
    
    * Add eslint file.
    
    * installed more packages for eslint
    
    ---------
    
    Co-authored-by: Han kyung Sung <hankyungsung@Hans-MacBook-Pro.local>
    
    * Hansung/add eslint (#21)
    
    * Updated .gitignore file
    
    * Add eslint file.
    
    * installed more packages for eslint
    
    * Update package.json command and pipeline.
    
    ---------
    
    Co-authored-by: Han kyung Sung <hankyungsung@Hans-MacBook-Pro.local>
    
    * Hansung/fix eslint (#22)
    
    * Updated the eslint rule.
    
    * Fixed the eslint
    
    ---------
    
    Co-authored-by: Han kyung Sung <hankyungsung@Hans-MacBook-Pro.local>
    
    * Delete .github/workflows/eslint.yml
    
    ---------
    
    Co-authored-by: Han kyung Sung <hankyungsung@Hans-MacBook-Pro.local>

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "jest"],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        trailingComma : "none"
+      },
+    ],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // or "error"
+      { 
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ],
+  },
+};
