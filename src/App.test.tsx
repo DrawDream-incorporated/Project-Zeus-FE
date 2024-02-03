9a9e9284f04d1cf9eda29c243919b9a080bcd334
kckoh <kyungcheolkoh@gmail.com>
2024-02-03 15:35:37 -0800

    add some comments in unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,12 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe('App', () => {
-    it("should find Konada Title", async () => { // Use async here
+    it("should find Konada Title", async () => { 
+        // Arrange
         render(<App />);
+        // Act
+
+        // Assert
         const appElements = await screen.findAllByTitle('Konada'); // Use await with findAllByTitle
         expect(appElements.length).toBeGreaterThan(0); // Check that the array is not empty
     });
