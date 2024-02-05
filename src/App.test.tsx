bd68ad0ff659d0bf64dbc3c46f7254f32aae8c23
kckoh <kyungcheolkoh@gmail.com>
2024-02-04 21:46:33 -0800

    fix eslint error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('App', () => {
-    it("should find Konada Title", async () => { 
-        // Arrange
-        render(<App />);
-        // Act
+describe("App", () => {
+  it("should find Konada Title", async () => {
+    // Arrange
+    render(<App />);
+    // Act
 
-        // Assert
-        const appElements = await screen.findAllByTitle('Konada'); // Use await with findAllByTitle
-        expect(appElements.length).toBeGreaterThan(0); // Check that the array is not empty
-    });
-
+    // Assert
+    const appElements = await screen.findAllByTitle("Konada"); // Use await with findAllByTitle
+    expect(appElements.length).toBeGreaterThan(0); // Check that the array is not empty
+  });
 });
