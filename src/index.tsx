c52ba33789b19781bf779128b7d3c02d82fd6f2a
YongjuLee <ivan.yongju.lee@gmail.com>
2024-02-21 21:51:22 -0800

    added postcss.config.cjs and installed postcss. I created dummyData.json file

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MantineProvider } from "@mantine/core";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
