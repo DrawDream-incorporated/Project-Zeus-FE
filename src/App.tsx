d56ee3438afb5d9cc870ef7a48f76f60e6d44774
YongjuLee <ivan.yongju.lee@gmail.com>
2024-03-12 21:18:03 -0700

    changed to original name

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/main/header/sheader";
+import Header from "./components/main/header/Header";
 function App() {
   return (
     <div className="App">
